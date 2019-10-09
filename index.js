4c3f64931a196672981a907e3bd3ac10ffd71050
Matthew <matthew.97.clark@gmail.com>
2019-10-09 15:00:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,14 +15,5 @@ function logWhisper(string) {
 }
 
 
-function sayHiToGrandma(string) {
-  if(string.toLowerCase() === lowercase) {
-    return "I can't hear you"
-  } else if(string === "I love you, Grandma.") {
-    return "I love you, too."
-  } else {
-    return "YES, INDEED!"
-  }
-}
 
 
