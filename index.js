a8fb3040216a8a7733c1516c3d2194d999bc8f3d
Matthew <matthew.97.clark@gmail.com>
2019-10-09 14:58:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string === lowercase) {
     return "I can't hear you"
+  } else if(string === "I love you, Grandma.") {
+    return "I love you, too."
+  } else {
+    return "YES, INDEED!"
   }
 }
 
